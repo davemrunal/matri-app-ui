d86ccd425ee291332108e9c8d4603ca9de6102ad
mrunaldave <davemrunal93@gmail.com>
2020-05-10 17:04:00 -0400

    Add styles and landing image for home

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,7 +4,7 @@ import './Home.css';
 export default function Home() {
     return (
         <div className="Home">
-            <div className="lander">
+            <div className="landingImage lander">
                 {/*<img src={require('../images/ganpati.jpg')} alt=""/>*/}
                 <h1>Coming Soon</h1>
                 <p>A modern way to find your match!</p>
