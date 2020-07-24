b25fd75c8bc1c64afb2bbbde0710a495cbdc32a1
mrunaldave <davemrunal93@gmail.com>
2020-07-23 23:46:04 -0400

    add homepage carousel

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,31 +1,43 @@
 import React from 'react';
 import './Home.css';
-import weddingImage from '../images/photo-1.jpeg';
-import weddingGif from '../images/editGif.gif';
+import weddingImage from '../images/dark-two.jpg';
+import weddingImage2 from '../images/destination-wedding1.jpg';
+import weddingImage3 from '../images/wedding-mandap.jpg';
+import weddingImage4 from '../images/tilt-two.jpeg';
+//import weddingImage from '../images/weddingGif-2.gif';
 
 export default function Home() {
     return (
         <div className="Home">
             <div className="container-fluid">
-                <div className="row">
-                    <img className="img-fluid w-100" src={weddingImage}/>
-                </div>
-                <div className="container-fluid about-us mb-3">
-                    <br/>
-                    <div className="row">
-                    </div>
-                    <div className="row justify-content-center">
-                        <div className="col-md-5 text-center">
-                            <h4 className="mr-5">Who Are We</h4>
-                            <p className="ml-5 w-75">We are the fastest growing matrimony company worldwide and our goal
-                                is
-                                to help people
-                                find their right match by providing best in class service.</p>
-                            <p className="ml-5 w-75">Finding a match and organizing events can seem very intimidating.
-                                We wil help you navigate this journey with personal care and customized ideas.</p>
+                <div className="row gif-row-height">
+                    <div className="col p-0">
+                        {/*<img className="img-fluid w-100" src={weddingImage}/>*/}
+                        <div id="carouselExampleSlidesOnly" className="carousel slide gif-row-height" data-ride="carousel">
+                            <div className="carousel-inner">
+                                <div className="carousel-item">
+                                    <img className="img-fluid w-100" src={weddingImage} alt="First slide"/>
+                                </div>
+                                <div className="carousel-item active">
+                                    <img className="img-fluid w-100" src={weddingImage2} alt="Second slide"/>
+                                </div>
+                                <div className="carousel-item">
+                                    <img className="img-fluid w-100" src={weddingImage3} alt="Third slide"/>
+                                </div>
+                                <div className="carousel-item">
+                                    <img className="img-fluid w-100" src={weddingImage4} alt="Fourth slide"/>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
+                <div className="row row-height">
+                    <div className="col-md-12 bg-secondary">
+                    </div>
+                </div>
+            </div>
+            <div className="jumbotron text-center mb-0">
+                <p>Footer</p>
             </div>
         </div>
     );
