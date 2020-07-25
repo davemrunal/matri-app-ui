774e2326a980b4f8b6a55f3f4f7811db76e26847
mrunaldave <davemrunal93@gmail.com>
2020-07-25 19:11:04 -0400

    add service cards to home

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -10,10 +10,11 @@ export default function Home() {
     return (
         <div className="Home">
             <div className="container-fluid">
-                <div className="row gif-row-height">
+                <div className="row">
                     <div className="col p-0">
                         {/*<img className="img-fluid w-100" src={weddingImage}/>*/}
-                        <div id="carouselExampleSlidesOnly" className="carousel slide gif-row-height" data-ride="carousel">
+                        <div id="carouselExampleSlidesOnly" className="carousel slide gif-row-height"
+                             data-ride="carousel">
                             <div className="carousel-inner">
                                 <div className="carousel-item">
                                     <img className="img-fluid w-100" src={weddingImage} alt="First slide"/>
@@ -31,8 +32,47 @@ export default function Home() {
                         </div>
                     </div>
                 </div>
-                <div className="row row-height">
-                    <div className="col-md-12 bg-secondary">
+                <div className="row second-row justify-content-center">
+                    <div className="row">
+                        <div className="col-md-12 mt-4">
+                            <h3 className="welcometext">More than just an online match-maker, we bring relations
+                                to life.</h3>
+                            <h4 className="welcomedetail">We are the fastest growing match-maker worldwide with one
+                                goal:
+                                <br/>
+                                helping people find their match</h4>
+                            <br/>
+                            <h3 className="services">
+                                <span className="services-border">SERVICES</span>
+                            </h3>
+                        </div>
+                    </div>
+                    <div className="row mx-auto mt-3">
+                        <div className="card-deck">
+                            <div className="card card-1">
+                                <div className="card-body">
+                                    <h3 className="card-title">Responsive Service</h3>
+                                    <p className="card-text">We provide best in class service with very high success
+                                        ratios
+                                        and are available to answer questions</p>
+                                </div>
+                            </div>
+                            <div className="card card-2">
+                                <div className="card-body">
+                                    <h3 className="card-title">Personalised Matching</h3>
+                                    <p className="card-text">We carefully note down your preferences and likings
+                                        and then provide you with curated choices of your potential matches</p>
+                                </div>
+                            </div>
+                            <div className="card card-3">
+                                <div className="card-body">
+                                    <h3>100% Verified Profiles</h3>
+                                    <p>All profiles registered with us are 100% verified manually by our staff
+                                        so you don't get any last minute surprises!
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
