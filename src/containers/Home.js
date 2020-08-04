c702def0561475fae73ad5778af206c727c9aeee
mrunaldave <davemrunal93@gmail.com>
2020-08-04 10:36:43 -0400

    Replace footer with simple one line

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -89,54 +89,8 @@ export default function Home() {
                         </ol>
                     </div>
                 </div>
-                <footer className="container py-5">
-                    <div className="row">
-                        <div className="col-6 col-md">
-                            <h5>Features</h5>
-                            <ul className="list-unstyled text-small">
-                                <li><a className="text-muted" href="#">Cool stuff</a></li>
-                                <li><a className="text-muted" href="#">Random feature</a></li>
-                                <li><a className="text-muted" href="#">Team feature</a></li>
-                                <li><a className="text-muted" href="#">Stuff for developers</a></li>
-                                <li><a className="text-muted" href="#">Another one</a></li>
-                                <li><a className="text-muted" href="#">Last time</a></li>
-                            </ul>
-                        </div>
-                        <div className="col-6 col-md">
-                            <h5>Resources</h5>
-                            <ul className="list-unstyled text-small">
-                                <li><a className="text-muted" href="#">Resource</a></li>
-                                <li><a className="text-muted" href="#">Resource name</a></li>
-                                <li><a className="text-muted" href="#">Another resource</a></li>
-                                <li><a className="text-muted" href="#">Final resource</a></li>
-                            </ul>
-                        </div>
-                        <div className="col-6 col-md">
-                            <h5>Resources</h5>
-                            <ul className="list-unstyled text-small">
-                                <li><a className="text-muted" href="#">Business</a></li>
-                                <li><a className="text-muted" href="#">Education</a></li>
-                                <li><a className="text-muted" href="#">Government</a></li>
-                                <li><a className="text-muted" href="#">Gaming</a></li>
-                            </ul>
-                        </div>
-                        <div className="col-6 col-md">
-                            <h5>About</h5>
-                            <ul className="list-unstyled text-small">
-                                <li><a className="text-muted" href="#">Team</a></li>
-                                <li><a className="text-muted" href="#">Locations</a></li>
-                                <li><a className="text-muted" href="#">Privacy</a></li>
-                                <li><a className="text-muted" href="#">Terms</a></li>
-                            </ul>
-                        </div>
-                        <div className="col-12 col-md ml-5">
-                            <img src={logo}/>
-                            <small className="d-block mb-3 text-muted ml-5">© 2017-2018</small>
-                        </div>
-                        {/*<div className="col-md-4 text-center mt-5">*/}
-                        {/*    <p className="lead">India | USA | Canada</p>*/}
-                        {/*</div>*/}
-                    </div>
+                <footer className="my-5 pt-5 text-muted text-center text-small">
+                    <p className="mb-1">© 2017-2018 Prime Matrimony</p>
                 </footer>
             </div>
         </div>
